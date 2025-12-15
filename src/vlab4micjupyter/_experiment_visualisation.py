ea2668d8db8ea041a7da9ec4e096c8c55561c0c9
Damian Martinez <jdmartinezigc@gmail.com>
2025-12-15 17:09:53 +0000

    update acq parameters in widgets to have all channels available

--- src/vlab4micjupyter/_experiment_visualisation.py
+++ src/vlab4micjupyter/_experiment_visualisation.py
@@ -502,6 +502,7 @@ def ui_set_acq_params(experiment):
         imager_channels.append(chann)
         channels.append(chann)
     nchannels = len(imager_channels)
+    experiment.reset_to_defaults(module="acquisitions", channels=imager_channels)
 
     def set_params(b):
         mod_id = acquisition_gui["modalities_dropdown"].value
@@ -651,7 +652,7 @@ def ui_set_acq_params(experiment):
     def clear(b):
         print("Acquisition parameters cleared")
         with io.capture_output() as captured:
-            experiment.reset_to_defaults(module="acquisitions", save=True)
+            experiment.reset_to_defaults(module="acquisitions", channels=imager_channels)
         acquisition_gui["current_parameters"].value = _mods_text_update(
             mods_text_base="Current acquisition parameters for modalities: ",
             mod_acq_params=experiment.selected_mods,
